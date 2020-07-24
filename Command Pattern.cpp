bceb6fbe4f47c03c0e69a295b918f16ee119e6e4
oucifkarim01 <oucifkarim01@gmail.com>
2020-07-24 22:19:36 +0100

    Command Pattern

---
+++ Command Pattern.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Problem
+- we want to perform queries on objects without
+have to know their structure
+• Consequences
++ decoupling between the object that calls and the one that
+executed
++ adding new commands is easy in the
+as far as modifying existing classes is not
+not necessary
+*/
+
+
+class Command {
+  public:
+    virtual void execute () = 0;
+};
+
+
+class LampReceiver {
+  private:
+    bool lampActive = false;
+  public:
+    void turnOn(){
+      cout << "Lamp Turned On ."<<endl;
+      this->lampActive = true;
+    }
+    void turnOff(){
+      cout << "Lamp Turned Off ."<<endl;
+      this->lampActive = false;
+    }
+};
+
+class TurnOnCommand:public Command {
+  private:
+    LampReceiver * lamp=nullptr;
+  public:
+    TurnOnCommand(LampReceiver * lamp):lamp(lamp){} 
+    void execute(){
+      this->lamp->turnOn();
+    }
+};
+
+class TurnOffCommand:public Command {
+  private:
+    LampReceiver * lamp=nullptr;
+  public:
+    TurnOffCommand(LampReceiver * lamp):lamp(lamp){} 
+    void execute(){
+      this->lamp->turnOff();
+    }
+};
+
+class LightInvoker{
+  private:
+    Command * turnOn = nullptr;
+    Command * turnOff = nullptr;
+  public:
+    LightInvoker(Command* turnOn,Command* turnOff):turnOn(turnOn),turnOff(turnOff){}
+    void turnLightOn(){
+      this->turnOn->execute();
+    }
+    void turnLightOff(){
+      this->turnOff->execute();
+    }    
+};
+
+
+int main(){
+  LampReceiver * lampRec = new LampReceiver();
+  Command * turnOnCommand = new TurnOnCommand(lampRec);
+  Command * turnOffCommand = new TurnOffCommand(lampRec);
+  LightInvoker * lightInvoke = new LightInvoker(turnOnCommand,turnOffCommand);
+  
+  lightInvoke->turnLightOn();
+  lightInvoke->turnLightOff();
+  
+  
+  return 0;
+}
