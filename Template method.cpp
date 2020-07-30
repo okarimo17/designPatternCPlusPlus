dbb8ca20c22803db0d37322502bc9b73a61d8fbf
oucifkarim01 <oucifkarim01@gmail.com>
2020-07-30 22:41:29 +0100

    Template def

--- Template method.cpp
+++ Template method.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+/*
+"Define the skeleton of an algorithm in an operation, deferring some steps to subclasses.
+Template Method lets subclasses redefine certain steps of an algorithm without changing
+the algorithm's structure."
+gof def
+
+*/
 
 
 class AbstractArticleTemplate {
