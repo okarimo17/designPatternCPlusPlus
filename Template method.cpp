34eabefafb68a62a64f98f8cdd82f873b75ad228
oucifkarim01 <oucifkarim01@gmail.com>
2020-07-31 21:50:36 +0100

    Template Method Pattern

--- Template method.cpp
+++ Template method.cpp
@@ -6,8 +6,7 @@ using namespace std;
 "Define the skeleton of an algorithm in an operation, deferring some steps to subclasses.
 Template Method lets subclasses redefine certain steps of an algorithm without changing
 the algorithm's structure."
-gof def
-
+gof book definition
 */
 
 
